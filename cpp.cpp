ad2da62f62b600eeb352af20d354a38be5ab94f9
3juhwan <13selfesteem91@naver.com>
2022-07-30 14:37:40 +0900

    update cpp

--- cpp.cpp
+++ cpp.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
-#define x first
-#define y second
+#define FAST ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define INF 1e9+7
+#define MOD 1e9+7
 using namespace std;
 
 typedef long long ll;
@@ -36,6 +37,48 @@ ll sum(int left, int right) {
 }
 
 
+/* Segment Tree with lazy propagation */
+
+const int treeSize = 1e6+10;
+
+ll tree[treeSize*4];
+ll lazy[treeSize*4];
+
+void update_lazy(int x, int s, int e) {
+	if (lazy[x] != 0) {
+		tree[x] += (e - s + 1)*lazy[x];
+		if (s != e) {
+			lazy[x << 1] += lazy[x];
+			lazy[(x << 1) + 1] += lazy[x];
+		}
+		lazy[x] = 0;
+	}
+}
+
+void update_range(int x, int s, int e, int l, int r, ll diff) {
+	update_lazy(x, s, e);
+	if (e < l || s > r) return;
+	if (s >= l && e <= r) {
+		tree[x] += (e - s + 1)*diff;
+		if (s != e) {
+			lazy[x << 1] += diff;
+			lazy[(x << 1) + 1] += diff;
+		}
+		return;
+	}
+	update_range(x << 1, s, (s + e) / 2, l, r, diff);
+	update_range((x << 1) + 1, (s + e) / 2 + 1, e, l, r, diff);
+	tree[x] = tree[x << 1] + tree[(x << 1) + 1];
+}
+
+ll get(int x, int s, int e, int l, int r) {
+	update_lazy(x, s, e);
+	if (s > r || e < l) return 0;
+	if (s >= l && e <= r) return tree[x];
+	return get(x * 2, s, (s + e) / 2, l, r) + get(x * 2 + 1, (s + e) / 2 + 1, e, l, r);
+}
+
+
 /*     Compress     */
 
 vector<ll> i_pos, j_pos;
@@ -47,8 +90,8 @@ void compress(){
 	sort(j_pos.begin(), j_pos.end());
 	j_pos.erase(unique(j_pos.begin(), j_pos.end()), j_pos.end());
 	for (int i = 0; i < v.size(); i++){
-		v[i].x = lower_bound(i_pos.begin(), i_pos.end(), v[i].x) - i_pos.begin();
-		v[i].y = lower_bound(j_pos.begin(), j_pos.end(), v[i].y) - j_pos.begin();
+		v[i].first = lower_bound(i_pos.begin(), i_pos.end(), v[i].first) - i_pos.begin();
+		v[i].second = lower_bound(j_pos.begin(), j_pos.end(), v[i].second) - j_pos.begin();
 	}
 }
 
@@ -57,5 +100,5 @@ void compress(){
 
 int main()
 {
-    ios_base::sync_with_stdio(0), cin.tie(0);
+    FAST;
 }
